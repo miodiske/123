0a8398826005aabf58f2ec9f43ca6aa4c09acbf3
miodiske <51440655+miodiske@users.noreply.github.com>
2021-10-21 16:38:51 +0800

    online-taxi

---
+++ service-verification-code/src/main/java/com/lvxp/serviceverificationcode/controller/VetifyCodeController.java
@@ -0,0 +1,14 @@
+package com.lvxp.serviceverificationcode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("verify-code")
+public class VetifyCodeController {
+
+    public String generate(){
+
+    }
+
+}
